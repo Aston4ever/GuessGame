2d766fe52b967f58487b31076ab267306da38e8f
aston4ever <aston4ever@gmail.com>
2024-01-29 12:54:20 +0300

    update: add logic to check number correct

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
 
-
-
-
+const secretNumber  = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", ()=>{
-  const guess = document.querySelector(".guess").value;
+  const guess = +(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
+  } else  if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct number!"
+  } else  if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "To High!!!"
+  } else  if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "To Low!!!"
   }
 })
