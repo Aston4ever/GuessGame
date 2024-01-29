86bf6ddf7ee8da4a703aa33740a5135b039ef4f5
aston4ever <aston4ever@gmail.com>
2024-01-29 13:22:44 +0300

    complete: basic logic of the game

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 
 const secretNumber  = Math.trunc(Math.random() * 20) + 1;
 let score = 20
-document.querySelector(".number").textContent = secretNumber;
+
 
 document.querySelector(".check").addEventListener("click", ()=>{
   const guess = +(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
   } else  if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct number!"
+    document.querySelector(".message").textContent = "Correct number!";
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
   } else  if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "To High!!!";
