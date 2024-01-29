47db69dc5e147d0ecddde61284936a5c1a34184a
aston4ever <aston4ever@gmail.com>
2024-01-29 12:32:36 +0300

    update: changing values and content

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 'use strict';
 
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!";
+document.querySelector(".score").textContent = 13;
+document.querySelector(".number").textContent = 10;
+document.querySelector(".guess").value = 20;
+
