4af1fa57874f60a305f022f91f850439179bf3a3
aston4ever <aston4ever@gmail.com>
2024-01-29 13:11:55 +0300

    complete: logic score + content

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNumber  = Math.trunc(Math.random() * 20) + 1;
+let score = 20
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", ()=>{
@@ -10,8 +11,24 @@ document.querySelector(".check").addEventListener("click", ()=>{
   } else  if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct number!"
   } else  if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "To High!!!"
+    if (score > 1) {
+      document.querySelector(".message").textContent = "To High!!!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lose the game";
+      document.querySelector(".score").textContent = 0;
+    }
+
   } else  if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "To Low!!!"
+    if (score > 1) {
+      document.querySelector(".message").textContent = "To Low!!!"
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else  {
+      document.querySelector(".message").textContent = "You lose the game";
+      document.querySelector(".score").textContent = 0;
+    }
+
   }
 })
